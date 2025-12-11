2623c42c9e26c77e61cdc1a712dac22281721952
Letienduy2811 <leeduy2811@gmail.com>
2025-12-11 15:32:43 +0700

    Update BAI 5.7.py

--- bai5/BAI 5.7.py
+++ bai5/BAI 5.7.py
@@ -1,5 +1,5 @@
-print("Sinh vien: LE HOANG VU")
-print("Ma so SV : 245751030110084")
+print("Sinh vien: LE TIEN DUY")
+print("Ma so SV : 245752021610097")
 print("#############################")
 import tkinter
 import random
@@ -66,3 +66,4 @@ e.pack()
 e.focus_set()
 
 root.mainloop()
+
